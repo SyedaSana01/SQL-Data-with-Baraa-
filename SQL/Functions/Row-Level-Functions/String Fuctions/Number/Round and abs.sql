2b32958795f9064189946563cad42819355ed9dc
Syeda Suhasana <164773519+SyedaSana01@users.noreply.github.com>
2026-08-11 13:05:12 +0530

    Add files via upload

---
+++ SQL/Functions/Row-Level-Functions/String Fuctions/Number/Round and abs.sql
@@ -0,0 +1,18 @@
+
+-- Number Function
+
+--1. ROUND
+SELECT 3.516,
+ROUND(3.516,1) as round_1
+
+SELECT 3.516,
+round(3.516,0) as round_0
+
+select 3.516,
+round(3.516,2) as round_2
+
+--2.ABS
+SELECT -10,
+ABS(-10)
+
+
