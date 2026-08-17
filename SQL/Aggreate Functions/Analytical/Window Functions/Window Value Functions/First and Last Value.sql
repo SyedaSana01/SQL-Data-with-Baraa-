5c2ff02839d31c7c229c82276a8a12bf1600e280
Syeda Suhasana <164773519+SyedaSana01@users.noreply.github.com>
2026-08-17 18:21:27 +0530

    Update First and Last Value.sql

--- SQL/Aggreate Functions/Analytical/Window Functions/Window Value Functions/First and Last Value.sql
+++ SQL/Aggreate Functions/Analytical/Window Functions/Window Value Functions/First and Last Value.sql
@@ -6,4 +6,12 @@ Last_Value(Sales)Over (PARTITION BY ProductID ORDER BY Sales
 ROWS BETWEEN Current Row AND Unbounded Following) highestSales
 FROM Sales.Orders
 
--- 
+-- Find the difference between the current and the lowest sale
+SELECT *,
+Sales - MinSales as Diff
+FROM 
+(
+SELECT OrderID, ProductID,Sales,
+MIN(Sales) OVER (Partition BY ProductID)as MinSales
+FROM Sales.Orders
+)t
