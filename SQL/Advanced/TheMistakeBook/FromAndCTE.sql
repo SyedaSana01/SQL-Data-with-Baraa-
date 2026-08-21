316242f1d086f84fc5f8b17469cfb9495d4465b2
Syeda Suhasana <164773519+SyedaSana01@users.noreply.github.com>
2026-08-21 20:34:58 +0530

    Add files via upload

---
+++ SQL/Advanced/TheMistakeBook/FromAndCTE.sql
@@ -0,0 +1,46 @@
+-- FROM CLAUSE 
+
+--1 Find the customer(s) who have the highest total sales from Sales.Orders.
+SELECT *
+FROM
+(
+SELECT CustomerID,
+SUM(Sales) as totalSales
+FROM Sales.Orders
+GROUP BY CustomerID
+)t
+WHERE totalSales = (SELECT MAX(totalSales)
+FROM 
+(
+ SELECT CustomerID,
+ SUM(Sales) AS TotalSales
+ FROM Sales.Orders
+ GROUP BY CustomerID
+ )AS x
+ );
+
+ --LETS SOLVE THIS USING CTE 
+ WITH CTE_TotalSales AS 
+(
+SELECT CustomerID,
+SUM(Sales) as totalSales
+FROM Sales.Orders
+GROUP BY CustomerID
+)
+, CTE_MaxSales AS
+(
+SELECT 
+MAX(totalSales) MaxSale
+FROM CTE_TotalSales 
+)
+SELECT 
+ts.CustomerID,ts.TotalSales
+FROM CTE_TotalSales as ts
+JOIN CTE_MaxSales as m
+ON ts.TotalSales = m.MAXSale
+
+
+
+
+
+
