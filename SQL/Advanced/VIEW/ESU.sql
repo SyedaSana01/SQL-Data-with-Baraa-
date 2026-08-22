982b66c16601414b49224b1185316276cbd2a68c
Syeda Suhasana <164773519+SyedaSana01@users.noreply.github.com>
2026-08-22 15:47:47 +0530

    Add files via upload

---
+++ SQL/Advanced/VIEW/ESU.sql
@@ -0,0 +1,23 @@
+-- Provide a view for the EU Sales Team that combines details from all tables and excludes data related to USA
+CREATE VIEW Sales.V_Order_Details_Updated AS 
+(
+SELECT 
+o.OrderID,
+OrderDate,
+p.Product,
+p.Category,
+COALESCE (c.FirstName, '')+ ' ' +COALESCE( c.LastName , '' ) CustomerName,
+c.Country CustomerCountry,
+COALESCE (e.FirstName, '')+ ' ' +COALESCE( e.LastName , '' ) EmployeeName,
+e.Department,
+o.Sales,
+o.Quantity
+FROM Sales.Orders o 
+LEFT JOIN Sales.Products p 
+ON p.ProductID= o.ProductID
+LEFT JOIN Sales.Customers c 
+ON c.CustomerID= o.CustomerID
+LEFT JOIN Sales.Employees e
+ON e.EmployeeID = O.SalesPersonID
+WHERE c.Country != 'USA'
+)
